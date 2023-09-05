f4d495d560684f3705be9d3a9b15c8da4d26c4d8
Angel Uray <aeucampos@gmail.com>
2023-09-05 00:51:54 -0400

    Update schema with new migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_04_050639) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_05_005927) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,4 +27,34 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_04_050639) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "projects", force: :cascade do |t|
+    t.string "project_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "task_types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "task_name"
+    t.string "task_description"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.string "duration"
+    t.integer "parent_unique_id"
+    t.integer "unique_task_id"
+    t.bigint "project_id", null: false
+    t.bigint "task_type_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_tasks_on_project_id"
+    t.index ["task_type_id"], name: "index_tasks_on_task_type_id"
+  end
+
+  add_foreign_key "tasks", "projects"
+  add_foreign_key "tasks", "task_types"
 end
