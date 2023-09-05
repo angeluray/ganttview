64119040e1405cb6eea5e2f0cb6256de25a98b84
Angel Uray <aeucampos@gmail.com>
2023-09-05 00:46:05 -0400

    Fix file converter

--- app/controllers/diagrams_controller.rb
+++ app/controllers/diagrams_controller.rb
@@ -21,7 +21,37 @@ class DiagramsController < ApplicationController
 
   # POST /diagrams or /diagrams.json
   def create
-    @diagram = Diagram.new(diagram_params)
+    # @diagram = Diagram.new(diagram_params)
+    uploaded_file = params[:diagram][:mpp_file]
+
+    if uploaded_file.present?
+      puts "holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+      # Defines a temporary file path to store the uploaded file
+      temp_file_path = Rails.root.join('tmp', 'uploaded_mpp_file.mpp')
+  
+      # Save the uploaded file to the temporary location
+      File.open(temp_file_path, 'wb') do |file|
+        file.write(uploaded_file.read)
+      end
+      
+      project = MPXJ::Reader.read(temp_file_path)
+      puts "There are #{project.all_tasks.size} tasks in this project"
+      puts "There are #{project.all_resources.size} resources in this project"
+
+      puts "The resources are:"
+      project.all_resources.each do |resource|
+        puts resource.name
+      end
+
+      puts "The tasks are:"
+      project.all_tasks.each do |task|
+        puts "ID: #{task.unique_id}||||| Name: #{task.name}:  parent_project: #{task.parent_project}, parent_task: #{task.parent_task_unique_id}"
+        # puts "#{task.name}: starts on #{task.start}, finishes on #{task.finish}, it's duration is #{task.duration}, parent: #{task.parent_project}, assignments: #{task.assignments}, predecessors: #{task.predecessors}, successors: #{task.successors}, childs: #{task.child_tasks}, parent_task: #{task.parent_task}"
+      end
+      
+      File.delete(temp_file_path)
+    end
 
     respond_to do |format|
       if @diagram.save
@@ -65,6 +95,6 @@ class DiagramsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def diagram_params
-      params.require(:diagram).permit(:task_name, :description, :start_date, :end_date, :progress, :duration, :priority, :parent)
+      params.require(:diagram).permit(:file, :task_name, :description, :start_date, :end_date, :progress, :duration, :priority, :parent)
     end
 end
