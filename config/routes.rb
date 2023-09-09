d19512885665af33e5d10c90cb5dad5a3e5e3afd
Angel Uray <aeucampos@gmail.com>
2023-09-09 06:42:16 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  resources :task_types
-  resources :tasks
   resources :projects
-  resources :diagrams
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "projects#new"
+  root "projects#index"
 end
