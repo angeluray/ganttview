d9d97f0465897f7be07459da5278f8ccaed2a2c9
Angel Uray <aeucampos@gmail.com>
2023-09-09 06:52:45 -0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+TaskType.create(name: "Project")
+TaskType.create(name: "Task")
+TaskType.create(name: "Milestone")
