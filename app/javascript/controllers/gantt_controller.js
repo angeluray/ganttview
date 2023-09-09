e4492a7d6ba54e4b595dc1ecd51029b39d479128
Angel Uray <aeucampos@gmail.com>
2023-09-09 04:23:30 -0400

    Add comments and remove white spaces

--- app/javascript/controllers/gantt_controller.js
+++ app/javascript/controllers/gantt_controller.js
@@ -5,23 +5,22 @@ export default class extends Controller {
   connect() {
     const ganttData = this.element.getAttribute("data-gantt-data");
 
-    // Displays the object 
-    // console.log(ganttData);
-
     // Set gantt configuration and editors
     const textEditor = { type: "text", map_to: "text" };
 
     const startDateEditor = {
-        type: "date", map_to: "start_date", min: new Date(2000, 0, 1),
-        max: new Date(2024, 0, 1)
-    };
+        type: "date", map_to: "start_date", min: new Date(2000, 0, 1), // Since the .mpp has very old tasks...
+        max: new Date(2024, 0, 1)                                      // the limits are between the 2000's and 2024
+    };                                                                  // Normally they're set between the current year and the next one.
 
     const endDateEditor = {
         type: "date", map_to: "end_date", min: new Date(2000, 0, 1),
         max: new Date(2024, 0, 1)
     };
+    
     const durationEditor = { type: "number", map_to: "duration", min: 0, max: 100 };
     
+    // Add custom cells
     gantt.config.columns = [
       {name: "text", tree: true, width: '*', resize: true, editor: textEditor},
       {name: "start_date", align: "center", resize: true, editor: startDateEditor},
@@ -29,6 +28,7 @@ export default class extends Controller {
       {name: "duration", align: "center", editor: durationEditor}
     ];
 
+    // Format the calendar visualization
     gantt.config.scales = [
       {unit: "month", step: 1, format: "%F, %Y"},
       {unit: "day", step: 1, format: "%D, %j"}
@@ -43,7 +43,7 @@ export default class extends Controller {
 
     gantt.init("gantt_container");
     
-    // Make an Ajax request to the controller action
+    // Makes an ajax request to the controller action "Show"
     fetch(`/projects/${ganttData}.json`)
       .then((response) => {
         if (!response.ok) {
@@ -52,6 +52,7 @@ export default class extends Controller {
         return response.json();
       })
       .then((data) => {
+        // console.log("This is the data", data);
         gantt.parse(data); // Receives the object of hashes
       })
       .catch((error) => {
@@ -59,8 +60,8 @@ export default class extends Controller {
       });
     
 
+      // Handles task's cell edition from the interface
       gantt.attachEvent("onAfterTaskUpdate", function(id, task){
-        console.log("HIIIIIIII");
         console.log(id, task)
 
         const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
@@ -69,20 +70,19 @@ export default class extends Controller {
           method: "PATCH",
           headers: {
             "Content-Type": "application/json",
-            "X-CSRF-Token": csrfToken  // Adjust the content type if needed
+            "X-CSRF-Token": csrfToken // Avoids cross-origin issues
           },
-          body: JSON.stringify(task), // Convert data to JSON if needed
+          body: JSON.stringify(task),
         })
         .then(response => {
           if (!response.ok) {
-            console.log("Here", response)
+            // console.log("Here", response)
             throw new Error("Network response was not ok");
           }
-          // return response.json(); // Parse the response as JSON if needed
           return response.text();
         }).then(responseText => {
           if (responseText) {
-            const responseData = JSON.parse(responseText); // Parse the response as JSON
+            const responseData = JSON.parse(responseText);
             console.log("Success:", responseData);
           } else {
             console.log("Response data is empty");
