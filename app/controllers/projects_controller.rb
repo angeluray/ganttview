33ae7d8febef562e0786b27050f0859e3c06ae73
Angel Uray <aeucampos@gmail.com>
2023-09-08 21:03:06 -0400

    Add "Project" controller actions logic to extract file data

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,33 +1,52 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: %i[ show edit update destroy ]
 
-  # GET /projects or /projects.json
   def index
+    @project = Project.new
     @projects = Project.all
   end
 
-  # GET /projects/1 or /projects/1.json
   def show
+    # Creates object of hashes to be consumed by the stimulus controller "gantt.controller.js"
+    @project_tasks_data = { 
+      "data" =>
+      @project.tasks.map do |task|
+        {
+            id: task.unique_task_id == 0 ? 999 : task.unique_task_id,
+            text: task.task_name,
+            start_date: task.start_date.strftime('%d-%m-%Y'), # Extracts the date
+            duration: task.duration,
+            type: task.parent_unique_id == 0 ? "project" : nil,
+            open: task.parent_unique_id == 0 ? true : nil,
+            parent: task.task_type_id == 2 ? task.parent_unique_id : 0
+          }.compact
+        end,
+        "links" =>
+        @project.tasks.where("task_type_id = 2 OR relation IS NOT NULL").pluck(:unique_task_id, :relation).map do |unique_task_id, relation|
+          {
+            id: unique_task_id,
+            source: relation,
+            target: unique_task_id,
+            type: 0
+          }
+        end
+      }
+      # This object is used by gantt.parse to be able to display the project tasks.
+
+      respond_to do |format|
+        format.html # Render the show view
+        format.json { render json: @project_tasks_data } # Render @project_tasks_data as JSON whenever stimulus send the GET request
+      end
   end
 
-  # GET /projects/new
   def new
     @project = Project.new
   end
 
-  # GET /projects/1/edit
-  def edit
-  end
-
-  # POST /projects or /projects.json
   def create
-    # @project = Project.new(project_params)
-
-    #  # @diagram = Diagram.new(diagram_params)
      uploaded_file = params[:project][:mpp_file]
 
-     if uploaded_file.present?
-       puts "holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    if uploaded_file.present?
  
        # Defines a temporary file path to store the uploaded file
        temp_file_path = Rails.root.join('tmp', 'uploaded_mpp_file.mpp')
@@ -37,66 +56,88 @@ class ProjectsController < ApplicationController
          file.write(uploaded_file.read)
        end
        
+      # Use MPXJ gem to get the values from the .mpp file
        project = MPXJ::Reader.read(temp_file_path)
-       puts "There are #{project.all_tasks.size} tasks in this project"
-       puts "There are #{project.all_resources.size} resources in this project"
- 
-       puts "The resources are:"
-       project.all_resources.each do |resource|
-         puts resource.name
-       end
- 
-       puts "The tasks are:"
-       project.all_tasks.each do |task|
-         puts "ID: #{task.unique_id}||||| Name: #{task.name}:  parent_project: #{task.parent_project}, parent_task: #{task.parent_task_unique_id}"
-         # puts "#{task.name}: starts on #{task.start}, finishes on #{task.finish}, it's duration is #{task.duration}, parent: #{task.parent_project}, assignments: #{task.assignments}, predecessors: #{task.predecessors}, successors: #{task.successors}, childs: #{task.child_tasks}, parent_task: #{task.parent_task}"
-       end
 
-       @project = Project.new(project_name: "New project")
+      # The comments below were left on purpose for future development enhancements
+      # puts "#{project.all_tasks.size} tasks are present this project"
+ 
+      #  puts "The project's tasks are:"
+      #  project.all_tasks.each do |task|
+      #  puts "ID: #{task.unique_id}||||| Name: #{task.name}:  parent_project: #{task.parent_project}, parent_task: #{task.parent_task_unique_id}, start: #{task.start.to_date}, end: #{task.finish.to_date}, actual: #{task.actual_start.to_date}, early: #{task.early_start.to_date}, late: #{task.late_start.to_date}, late-f: #{task.late_finish.to_date}, early-f: #{task.early_finish.to_date}"
+         
+      # puts "this is #{task.predecessors}"
+      # if task.predecessors.present?
+      # task.predecessors.each do |relation|
+      #     puts relation.methods
+      # puts "it's related to #{relation.task_unique_id}}"
+      #  predecessor_id = predecessor.unique_id  # Adjust the key if necessary
+      #  puts "Has >>>#{predecessor.name}<<< as its Predecessor with an ID: #{predecessor.unique_id}"
+      # end
+      #  end
+      #  puts "#{task.name}: starts on #{task.start}, finishes on #{task.finish}, it's duration is #{task.duration}, parent: #{task.parent_project}, assignments: #{task.assignments}, predecessors: #{task.predecessors}, successors: #{task.successors}, childs: #{task.child_tasks}, parent_task: #{task.parent_task}"
+      #  puts "-----------------END OF TASK INFORMATION-----------------"
+      #  end
 
-       project.all_tasks.each do |task|
-        task_type_id = (task.unique_id == 0) ? 1 : 2
+      # Assigns a defaults project name
+      @project = Project.new(project_name: "New project")
+
+      project.all_tasks.each do |task|
+
+        # Task with parent_task_unique_id equal to zero are "projects" otherwise are normal tasks.
+        task_type_id = (task.parent_task_unique_id == 0) ? 1 : 2
+
+        # Increase the end-date 1 day to match visually the exact date of project ending.
+        project_end_date = task.finish + 1.day
+
+        duration = (project_end_date.to_date - task.start.to_date).to_i
+
+        # Creates variable to store ids of relations by hierarchy
+        predecessor_task_id = nil
+
+        # Organize the relations within the hierarchy
+        if task.predecessors.present?
+          task.predecessors.each do |relation|
+            predecessor_task_id = relation.task_unique_id
+          end
+        end
 
         @project.tasks.new(
           task_name: task.name,
           start_date: task.start,
-          end_date: task.finish,
-          duration: task.duration,
+          end_date: project_end_date,
+          duration: duration,
           parent_unique_id: task.parent_task_unique_id,
           unique_task_id: task.unique_id,
-          task_type_id: task_type_id
+          task_type_id: task_type_id,
+          relation: predecessor_task_id
         )
-       end
-       
-      end
-      
-      respond_to do |format|
-       if @project.save
-          File.delete(temp_file_path)
-          format.html { redirect_to project_url(@project), notice: "Project was successfully created." }
-          format.json { render :show, status: :created, location: @project }
-        else
-          format.html { render :new, status: :unprocessable_entity }
-          format.json { render json: @project.errors, status: :unprocessable_entity }
-        end
-      end
-      
-  end
 
-  # PATCH/PUT /projects/1 or /projects/1.json
-  def update
+        # Reset the value to bring space for the new one
+        predecessor_task_id = nil
+      end
+    end 
+      
     respond_to do |format|
-      if @project.update(project_params)
-        format.html { redirect_to project_url(@project), notice: "Project was successfully updated." }
-        format.json { render :show, status: :ok, location: @project }
+      if @project.save
+        File.delete(temp_file_path) # Delete temporal file to avoid memory consuming 
+        format.html { redirect_to project_url(@project), notice: "Project was successfully created." }
+        format.json { render :show, status: :created, location: @project }
       else
-        format.html { render :edit, status: :unprocessable_entity }
+        format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @project.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /projects/1 or /projects/1.json
+  def update
+    if project_params.present?
+      update_project
+    else
+      update_task
+    end
+  end
+
   def destroy
     @project.destroy
 
@@ -112,7 +153,62 @@ class ProjectsController < ApplicationController
       @project = Project.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+    # Encapsulates project's name update logic.
+    def update_project
+      respond_to do |format|
+        if @project.update(project_params)
+          format.html { redirect_to project_url(@project), notice: "Project's name successfully changed." }
+          format.json { render :show, status: :ok, location: @project }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @project.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    # Encapsulates Task's fields updates logic.
+    def update_task
+      # Parse the JSON data from the request body
+      task_data = JSON.parse(request.body.read)
+
+      # Parse dates from strf format to ISO Standard
+      # to match the start/end columns data types within the database
+      parsed_start_date = DateTime.parse(task_data['start_date'])
+      parsed_end_date = DateTime.parse(task_data['end_date'])
+
+      # unique_task_id contains the id assigned to the task from the .mpp file
+      # and it's use as the main reference.
+      @task = @project.tasks.find_by(unique_task_id: task_data['id']) 
+      
+      if @task # Checks wether the task exists or not
+
+        if @task.task_name != task_data['text']
+          @task.task_name = task_data['text']
+        end
+      
+        if @task.start_date != parsed_start_date
+          @task.start_date = parsed_start_date
+        end
+      
+        if @task.end_date != parsed_end_date
+          @task.end_date = parsed_end_date
+        end
+      
+        if @task.duration != task_data['duration']
+          @task.duration = task_data['duration']
+        end
+
+        if @task.save
+          # Returns a updated task into JSON format as a response
+          render json: @task, status: :ok
+        else
+          render json: { errors: @task.errors }, status: :unprocessable_entity
+        end
+      else
+        render json: { error: 'Task not found' }, status: :not_found
+      end
+    end
+
     def project_params
       params.require(:project).permit(:project_name)
     end
